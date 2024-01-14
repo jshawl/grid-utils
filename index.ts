dcf7a85a91d2a4696264c6cd5d6799da6a8b8fd8
Jesse Shawl <jesse@jesse.sh>
2024-01-14 15:13:53 -0600

    add columns

--- index.ts
+++ index.ts
@@ -6,82 +6,88 @@ type Options = {
   rows: number;
   columns?: number;
   initialize?: (col: number, row: number) => unknown;
-}
+};
 
 export const grid = ({
   rows,
   columns = rows,
-  initialize = (e) => e
+  initialize = (e) => e,
 }: Options) => {
-  return [...Array(rows)]
-    .map((row, rowi) => [...Array(columns)]
-      .map((col, coli) => initialize(coli, rowi)))
-}
+  return [...Array(rows)].map((row, rowi) =>
+    [...Array(columns)].map((col, coli) => initialize(coli, rowi)),
+  );
+};
 
 export const map = (grid: Grid, predicate: Predicate) => {
-  return grid.map((row, rowi) => row.map((col, coli) => {
-    return predicate(col, coli, rowi)
-  }))
-}
+  return grid.map((row, rowi) =>
+    row.map((col, coli) => {
+      return predicate(col, coli, rowi);
+    }),
+  );
+};
 
 export const each = (
   grid: unknown[][],
-  predicate: (col: unknown, coli: number, rowi: number) => unknown
+  predicate: (col: unknown, coli: number, rowi: number) => unknown,
 ) => {
-  map(grid, predicate)
-  return grid
-}
+  map(grid, predicate);
+  return grid;
+};
 
 export const parse = (text: string) => {
-  return text.trim().split("\n").map(row => row.split(""))
-}
+  return text
+    .trim()
+    .split("\n")
+    .map((row) => row.split(""));
+};
 
 export const inGrid = (grid: Grid, coord: number[]) => {
   const [row, col] = coord;
-  return col >= 0 && col < grid[0].length && row >= 0 && row < grid.length
-}
+  return col >= 0 && col < grid[0].length && row >= 0 && row < grid.length;
+};
 
 const directions = [
   [0, -1],
   [1, 0],
   [0, 1],
-  [-1, 0]
-]
+  [-1, 0],
+];
 
-export const neighbors = (grid: Grid, coord: number[]) => directions.reduce((acc: number[][], el) => {
-  const [col, row] = el
-  const [coordcol, coordrow] = coord
-  const neighborcol = coordcol + col
-  const neighborrow = coordrow + row
-  if (inGrid(grid, [neighborcol, neighborrow])) {
-    acc.push([neighborcol, neighborrow])
-  }
-  return acc
-}, []);
+export const neighbors = (grid: Grid, coord: number[]) =>
+  directions.reduce((acc: number[][], el) => {
+    const [col, row] = el;
+    const [coordcol, coordrow] = coord;
+    const neighborcol = coordcol + col;
+    const neighborrow = coordrow + row;
+    if (inGrid(grid, [neighborcol, neighborrow])) {
+      acc.push([neighborcol, neighborrow]);
+    }
+    return acc;
+  }, []);
 
 type Direction = (
   grid: Grid,
   [col, row]: number[],
-  all?: number[][]
-) => number[][]
+  all?: number[][],
+) => number[][];
 
 const direction = ([dcol, drow]: number[]) => {
-  const fn: Direction = (
-    grid,
-    [col, row],
-    all = []
-  ) => {
-    const next = [col + dcol, row + drow]
-    if (!inGrid(grid, next)) return all
-    return fn(grid, next, [...all, next])
-  }
-  return fn
-}
+  const fn: Direction = (grid, [col, row], all = []) => {
+    const next = [col + dcol, row + drow];
+    if (!inGrid(grid, next)) return all;
+    return fn(grid, next, [...all, next]);
+  };
+  return fn;
+};
 
-export const top = direction([0, -1])
+export const top = direction([0, -1]);
 
-export const right = direction([1, 0])
+export const right = direction([1, 0]);
 
-export const bottom = direction([0, 1])
+export const bottom = direction([0, 1]);
 
-export const left = direction([-1, 0])
+export const left = direction([-1, 0]);
+
+export const columns = (grid: Grid) => {
+  return grid[0].map((_, coli) => grid.map((_, rowi) => grid[rowi][coli]));
+};
